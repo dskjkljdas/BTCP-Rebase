9b07f02852f8b4c4576d70fa19296bffeeda2158
richardvt <42453559+dskjkljdas@users.noreply.github.com>
2019-01-20 10:53:20 +1100

    Update transaction.h

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -201,31 +201,32 @@ struct CMutableTransaction;
  */
 template<typename Stream, typename TxType>
 inline void UnserializeTransaction(TxType& tx, Stream& s) {
-    static_assert(0 < MIN_TX_VERSION, "dummy version must be a consensus invalid version");
 
-    const bool fAllowWitness = !(s.GetVersion() & SERIALIZE_TRANSACTION_NO_WITNESS);
 
+ const bool fAllowWitness = !(s.GetVersion() & SERIALIZE_TRANSACTION_NO_WITNESS);
+ s >> tx.nVersion;
     unsigned char flags = 0;
-    s >> tx.nVersion;
-    if (tx.nVersion == 0) {
-        /* We read a dummy version, read flags and then the real version */
-        s >> flags;
-        s >> tx.nVersion;
-    }
-
     tx.vin.clear();
     tx.vout.clear();
     s >> tx.vin;
-    s >> tx.vout;
-    if ((flags & 1)) {
+    if (tx.vin.size() == 0 && fAllowWitness) {
+        /* We read a dummy or an empty vin. */
+        s >> flags;
+        if (flags != 0) {
+            s >> tx.vin;
+            s >> tx.vout;
+        }
+    } else {
+        /* We read a non-empty vin. Assume a normal vout follows. */
+        s >> tx.vout;
+    }
+    if ((flags & 1) && fAllowWitness) {
+   
         /* The witness flag is present, and we support witnesses. */
         flags ^= 1;
         for (size_t i = 0; i < tx.vin.size(); i++) {
             s >> tx.vin[i].scriptWitness.stack;
-            if(!fAllowWitness) {
-                tx.vin[i].scriptWitness.stack.clear();
             }
-        }
     }
     if (flags) {
         /* Unknown flag in the serialization */
@@ -244,19 +245,23 @@ inline void UnserializeTransaction(TxType& tx, Stream& s) {
 template<typename Stream, typename TxType>
 inline void SerializeTransaction(const TxType& tx, Stream& s) {
     const bool fAllowWitness = !(s.GetVersion() & SERIALIZE_TRANSACTION_NO_WITNESS);
-
+    s << tx.nVersion;
     unsigned char flags = 0;
-    if (fAllowWitness && tx.HasWitness()) {
-        flags |= 1;
+    // Consistency check
+    if (fAllowWitness) {
+        /* Check whether witnesses need to be serialized. */
+        if (tx.HasWitness()) {
+            flags |= 1;
+        }
+    
     }
 
     if (flags) {
         /* Use extended format in case witnesses are to be serialized. */
-        s << 0;
+         std::vector<CTxIn> vinDummy;
+        s << vinDummy;
         s << flags;
     }
-
-    s << tx.nVersion;
     s << tx.vin;
     s << tx.vout;
     if (flags & 1) {
